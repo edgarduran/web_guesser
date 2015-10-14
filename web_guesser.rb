79abb5c36ebc3152a7342b11c5ff8a8c1f9405db
Edgar Duran <edgarduran@Edgars-MacBook-Pro.local>
2015-10-13 22:18:33 -0600

    made changes/practice

--- web_guesser.rb
+++ web_guesser.rb
@@ -4,22 +4,22 @@ require 'sinatra/reloader'
 NUMBER = rand(100)
 
 get '/' do
-  guess = params["guess"].to_i
-  evaluation = evaluate_guess(guess)
-  erb :index, :locals => {:number => NUMBER, :evaluation => evaluation }
+  guess = params[:guess]
+  message = check_guess(guess)
+  erb :index, :locals => {:number => NUMBER, :message => message}
 end
 
 def evaluate_guess(guess)
   if    guess >= NUMBER + 5
-        evaluation = "Your guess of #{guess} is way too high!!"
+        message = "Your guess of #{guess} is way too high!!"
   elsif guess <= NUMBER - 5
-        evaluation = "Your guess of #{guess} is way too low!!"
+        message = "Your guess of #{guess} is way too low!!"
   elsif guess > NUMBER
-        evaluation = "Your guess of #{guess} is too high!"
+        message = "Your guess of #{guess} is too high!"
   elsif guess < NUMBER
-        evaluation = "Your guess of #{guess} is too low!"
+        message = "Your guess of #{guess} is too low!"
   else  guess == NUMBER
-        evaluation ="The SECRET Number is #{NUMBER}.Your guess is Correct!!!"
+        message ="The SECRET Number is #{NUMBER}.Your guess is Correct!!!"
   end
-        evaluation
+        message
 end
